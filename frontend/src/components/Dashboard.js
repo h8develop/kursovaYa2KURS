1305662c5059dd1c3ed7d1ed4226d43c4d435721
David S <negeorgefloyd@gmail.com>
2024-12-10 05:10:59 +0300

    test metrics V4 (graph total ads

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -5,7 +5,7 @@ return (
 <div>
     <h2>Analytics Dashboard</h2>
     <iframe 
-        src="http://grafana.local/d/your_dashboard_id/your_dashboard_name?orgId=1&refresh=10s" 
+        src="http://grafana.local/d/ce6gdaqyi2eiof/total-ads?orgId=1&refresh=10s" 
         width="100%" 
         height="800px"
         style={{ border: 0 }}
