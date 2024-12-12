c1807ba83cf19843c3a8a6bd43bcdf8dec287f2c
David S <negeorgefloyd@gmail.com>
2024-12-12 15:45:06 +0300

    try fix grafana V2

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,7 +4,7 @@ function Dashboard() {
 return (
 <div>
     <h2>Analytics Dashboard</h2>
-    <iframe src="http://grafana.local/d-solo/ce6gdaqyi2eiof/total-ads?orgId=1&refresh=10s&from=1733798169477&to=1733798469478&timezone=browser&panelId=1&__feature.dashboardSceneSolo" width="450" height="200" frameborder="0"></iframe>
+    <iframe src="http://grafana.local/public-dashboards/88a41d22e115470eb2451314d96643fd" width="450" height="200" frameborder="0"></iframe>
 </div>
 );
 }
