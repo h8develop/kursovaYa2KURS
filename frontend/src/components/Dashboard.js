67cb9e6043491d8db86fa5c5ba244070e98eebc8
David S <negeorgefloyd@gmail.com>
2024-12-13 13:20:06 +0300

    FINALLY FIX grafana

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,7 +4,7 @@ function Dashboard() {
 return (
 <div>
     <h2>Analytics Dashboard</h2>
-    <iframe src="http://grafana.local/public-dashboards/88a41d22e115470eb2451314d96643fd" width="450" height="200" frameborder="0"></iframe>
+    <iframe src="http://grafana.local/public-dashboards/54a021cb6f4746ee8a2a39de82b1259c" width="450" height="200" frameborder="0"></iframe>
 </div>
 );
 }
