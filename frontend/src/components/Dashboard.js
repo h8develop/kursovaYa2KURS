418f50faa638fe71468c4e7c624312f988d1e708
David S <negeorgefloyd@gmail.com>
2024-12-13 16:48:48 +0300

    relise vers

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Dashboard() {
 return (
 <div>
-    <h2>Analytics Dashboard</h2>
-    <iframe src="http://grafana.local/public-dashboards/88a41d22e115470eb2451314d96643fd" width="800" height="500" frameborder="0"></iframe>
+    <h2>Графики для аналитики</h2>
+    <iframe src="http://grafana.local/public-dashboards/88a41d22e115470eb2451314d96643fd" width="600" height="400" frameborder="0"></iframe>
 </div>
 );
 }
